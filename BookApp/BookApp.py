e68a9b47cfc9407447337a36a6584863c5394272
MikkoJaakola <mikko.jaakola@hotmail.fi>
2021-05-31 00:10:10 +0300

    Added Login and Register funcionality

--- BookApp/BookApp.py
+++ BookApp/BookApp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -8,12 +8,98 @@ app = Flask(__name__)
 app.secret_key = "aez2Caipootohd2ahph1zie5aefoh0"
 db = SQLAlchemy(app)
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, PasswordField, validators
+
 class Book(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	book = db.Column(db.String, nullable=False)
 	plot = db.Column(db.Text, nullable=False)
 	
 BookForm = model_form(Book, base_class=FlaskForm, db_session=db.session)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String, nullable=False, unique=True)
+	passwordHash = db.Column(db.String, nullable=False)
+
+	def setPassword(self, password):
+		self.passwordHash = generate_password_hash(password)
+
+	def checkPassword(self, password):
+		return check_password_hash(self.passwordHash, password)
+		
+class UserForm(FlaskForm):
+	email = StringField("email", validators=[validators.Email()])
+	password = PasswordField("password", validators=[validators.InputRequired()])
+	
+def currentUser():
+	try:
+		uid = int(session["uid"])
+	except:
+		return None
+	return User.query.get(uid)
+
+app.jinja_env.globals["currentUser"] = currentUser
+
+@app.route("/user/login", methods=["GET", "POST"])
+def loginView():
+	form = UserForm()
+
+	if form.validate_on_submit():
+		email = form.email.data
+
+		password = form.password.data
+
+
+		user = User.query.filter_by(email=email).first()
+		if not user:
+			flash("Login failed.")
+			print("No such user")
+			return redirect("/user/login")
+		if not user.checkPassword(password):
+			flash("Login failed.")
+			print("Wrong password")
+			return redirect("/user/login")
+
+		session["uid"] = user.id
+
+		flash("Login successful.")
+		return redirect("/")
+		
+	return render_template("login.html", form=form)
+	
+@app.route("/user/register", methods=["GET", "POST"])
+def registerView():
+	form = UserForm()
+
+	if form.validate_on_submit():
+		email = form.email.data
+
+		password = form.password.data
+
+
+		if User.query.filter_by(email=email).first():
+			flash("User already exits! Please log in.")
+			return redirect("/user/login")
+
+		user = User(email=email)
+		user.setPassword(password)
+
+		db.session.add(user)
+		db.session.commit()
+
+		flash("Registration successfull")
+		return redirect("/user/login")
+
+	return render_template("register.html", form=form)
+	
+	
+@app.route("/user/logout")
+def logoutView():
+	session["uid"] = None
+	flash("Signed out")
+	return redirect("/")
 
 @app.before_first_request
 def initMe():
